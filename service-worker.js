610a0a9b8a2680cd5f5b0384de73d991292a0e81
Kihtrak Raknas <karthikandsankar@gmail.com>
2020-01-11 09:27:45 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/scoutr/precache-manifest.5fad40c4825d747abdd45a6f82e7d3b6.js"
+  "/scoutr/precache-manifest.07cb76e4490716984f568020341fa2c0.js"
 );
 
 workbox.skipWaiting();
